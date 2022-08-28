055638ebd42b54b362c8f1a80b70b10418d57928
Miqdad Farcha <miqdad.farcha@gmail.com>
2022-08-28 13:41:04 +0700

    update error middleware

--- src/middleware/http-error-middleware.js
+++ src/middleware/http-error-middleware.js
@@ -3,10 +3,13 @@ const BaseHandler = require('../http/base-handler');
 class ErrorMiddleware extends BaseHandler {
   handleMiddleware(err, req, res, next) {
     if (err) {
-      if (err.status === 422) {
-        return super.unprocessableEntityResponse(res, err.message, err.details);
-      } else {
-        return super.internalErrorResponse(res, err.message)
+      switch (err.status) {
+        case 400: return super.badRequest(res)
+        case 401: return super.unauthorized(res)
+        case 403: return super.forbidden(res)
+        case 404: return super.notFoundResponse(res)
+        case 422: return super.unprocessableEntityResponse(res, err.message, err.details)
+        default: return super.internalErrorResponse(res, err.message, err)
       }
     }
     return next();
