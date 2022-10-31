6ef5e6902d4f530c553052a8bbfcf715663785f5
Miqdad Farcha <miqdad.farcha@gmail.com>
2022-11-01 00:34:01 +0700

    Fix bad request message http-middleware

--- src/middleware/http-error-middleware.js
+++ src/middleware/http-error-middleware.js
@@ -4,7 +4,7 @@ class ErrorMiddleware extends BaseHandler {
   handleMiddleware(err, req, res, next) {
     if (err) {
       switch (err.status) {
-        case 400: return super.badRequest(res)
+        case 400: return super.badRequest(res, err.message)
         case 401: return super.unauthorized(res)
         case 403: return super.forbidden(res)
         case 404: return super.notFoundResponse(res, err.message)
